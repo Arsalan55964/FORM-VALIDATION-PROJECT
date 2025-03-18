b236bc963ec02bab8df1c088fcd734ef242fcae9
js-arsalan <oarsalan17@gmail.com>
2025-03-18 23:04:16 +0500

    done

---
+++ fliter-map.js
@@ -0,0 +1,21 @@
+// const arr = [1, 2, 3, 4, 5, 6]
+
+// const newarray = [7,8,9]
+
+// const myn1 = [arr + newarray]
+// console.log(myn1);
+
+
+// const myn2= arr.slice(1,6);
+// console.log( "b" , myn2);
+
+
+const arr = [1,2,3,4,5,6]
+
+const myarr = arr.pop(1,2,3,4,5)
+console.log(myarr);
+
+
+
+
+
